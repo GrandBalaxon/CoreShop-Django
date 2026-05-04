29dfcb2c56e7c67b4dde49b9d023ea5b9d24b911
Vlad Spinul <shinokakaov@mail.ru>
2026-05-04 05:55:48 +0300

    feat: succsesful POST on contacts/ now lead to message_received.html + improved its style

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -9,8 +8,8 @@ def index(request):
 def contacts(request):
     if request.method == 'POST':
         name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
+        context = {'name': name}
 
-        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
+        return render(request, "catalog/message_received.html", context)
+
     return render(request, 'catalog/contacts.html')
