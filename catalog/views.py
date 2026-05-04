ee268ecc4af6f3ded79b352e7c7088c1398e526a
Vlad Spinul <shinokakaov@mail.ru>
2026-05-05 02:15:09 +0300

    feat: added print of 5 latest products in index view

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
-from catalog.models import ContactInfo
+from catalog.models import ContactInfo, Product
 
 
 def index(request):
+    latest_products = Product.objects.order_by('-created_at')[:5]
+
+    print("Последние 5 продуктов:")
+    for product in latest_products:
+        print(f"* {product.name} — ${product.price} - Категория: {product.category}")
+
     return render(request, 'catalog/index.html')
 
 
