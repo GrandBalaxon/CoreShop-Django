1ec0f259e11e6fd54196b681bc90b4fc88f4aa34
Vlad Spinul <shinokakaov@mail.ru>
2026-05-07 21:55:12 +0300

    feat: image on Product model now can be null/blank + altered it's __str__ method

--- catalog/models.py
+++ catalog/models.py
@@ -16,7 +16,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name="Наименование")
     description = models.TextField(verbose_name="Описание")
-    image = models.ImageField(upload_to='images/', verbose_name="Изображение")
+    image = models.ImageField(upload_to='images/', null=True, blank=True, verbose_name="Изображение")
     category = models.ForeignKey(
         Category,
         on_delete=models.CASCADE,
@@ -28,7 +28,7 @@ class Product(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата последнего изменения")
 
     def __str__(self):
-        return f"{self.name} - {self.price} $"
+        return f"{self.name}: ${self.price}"
 
     class Meta:
         verbose_name = "товар"
