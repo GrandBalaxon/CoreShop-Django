0310027500d2592b3b217cb63bc0ada0d6b9dc14
Vlad Spinul <shinokakaov@mail.ru>
2026-05-08 00:07:22 +0300

    feat: altered load_test_data command, now it restarts ID's from 1

--- catalog/management/commands/load_test_data.py
+++ catalog/management/commands/load_test_data.py
@@ -1,5 +1,7 @@
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
+from django.db import connection
+
 from catalog.models import Category, Product
 
 class Command(BaseCommand):
@@ -8,6 +10,10 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         Product.objects.all().delete()
         Category.objects.all().delete()
+
+        with connection.cursor() as cursor:
+            cursor.execute("ALTER SEQUENCE catalog_product_id_seq RESTART WITH 1;")
+            cursor.execute("ALTER SEQUENCE catalog_category_id_seq RESTART WITH 1;")
 
         call_command('loaddata', 'catalog_fixture.json')
 
