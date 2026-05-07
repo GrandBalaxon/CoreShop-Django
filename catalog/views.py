5f8555ad6459281e91bb5df308502065a159c410
Vlad Spinul <shinokakaov@mail.ru>
2026-05-07 21:22:48 +0300

    feat: implemented POST method on 'add-product' + created new succsess page

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from catalog.models import ContactInfo, Product
+from catalog.models import ContactInfo, Product, Category
 
 
 def index(request):
@@ -32,4 +32,22 @@ def product_details(request, product_id):
 
 
 def add_product(request):
-    return render(request, 'catalog/add_product.html')
+    categories = Category.objects.all()
+    context = {'categories': categories}
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        price = request.POST.get('price')
+        category_id = request.POST.get('category')
+        description = request.POST.get('description')
+
+        new_product = Product.objects.create(
+            name=name,
+            price=price,
+            category_id=category_id,
+            description=description
+        )
+
+        return render(request, "catalog/product_added.html")
+
+    return render(request, 'catalog/add_product.html', context)
