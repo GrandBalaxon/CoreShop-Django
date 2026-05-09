67397dc878350d3cb856e1c01c63581be8439c62
Vlad Spinul <shinokakaov@mail.ru>
2026-05-10 00:36:11 +0300

    feat: added create blogpost button + it linked + created blog_details.html

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ app_name = BlogConfig.name
 
 urlpatterns = [
     path("", views.BlogPostsView.as_view(), name="blog"),
+    path("post/<int:pk>/", views.BlogPostDetailView.as_view(), name="blog_detail"),
+    path("post/new/", views.BlogPostCreateView.as_view(), name="blog_write"),
 ]
