d462e03a1334ec716a0652368758845326afd693
Vlad Spinul <shinokakaov@mail.ru>
2026-05-09 22:16:52 +0300

    feat: created and registered new 'blog' app

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
 from django.views.generic import ListView, TemplateView, DetailView
 from django.views.generic.edit import CreateView
 
