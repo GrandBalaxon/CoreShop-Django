117907a94642dc2e1c6615e063336510b8f6d431
Vlad Spinul <shinokakaov@mail.ru>
2026-05-09 23:59:22 +0300

    feat: added blog post's personel link for admin

--- blog/admin.py
+++ blog/admin.py
@@ -8,3 +8,10 @@ class BlogPostAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "created_at", "is_published", "views_count")
     list_filter = ("is_published",)
     search_fields = ("title",)
+
+    def link(self, obj):
+        url = obj.get_absolute_url()
+        return f'<a href="{url}" target="_blank">Смотреть</a>'
+
+    link.short_description = "Ссылка на статью"
+    link.allow_tags = True
--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class BlogPost(models.Model):
@@ -6,12 +7,16 @@ class BlogPost(models.Model):
     content = models.TextField(verbose_name="Содержимое")
     preview = models.ImageField(upload_to='images/blog/', null=True, blank=True, verbose_name="Превью")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
     is_published = models.BooleanField(default=False, verbose_name="Опубликовано")
-    views_count = models.IntegerField(verbose_name="Количество просмотров")
+    views_count = models.IntegerField(default=0, verbose_name="Количество просмотров")
 
     def __str__(self):
         is_published = "Опубликовано" if self.is_published else "Не опубликовано"
         return f"{self.title} - {is_published} - Просмотров: {self.views_count}"
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", kwargs={"pk": self.pk})
 
     class Meta:
         verbose_name = "статья"
