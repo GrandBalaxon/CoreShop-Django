5f4b64fc3e9b5425e0021042d56729ada7f898d1
Vlad Spinul <shinokakaov@mail.ru>
2026-05-10 00:30:39 +0300

    feat: added write_blog.html and it's CBV

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,43 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
 
-from blog.models import BlogPost
+from blog.models import BlogPost, Category
 
 
 class BlogPostsView(ListView):
     model = BlogPost
     template_name = 'blog/home.html'
     context_object_name = 'blogposts'
+    queryset = BlogPost.objects.filter(is_published=True)  # только опубликованные
+
+
+class BlogPostDetailView(DetailView):
+    model = BlogPost
+    template_name = "blog/blog_detail.html"
+    context_object_name = "blogpost"
+
+    def get_object(self, *args, **kwargs):
+        blogpost = super().get_object(*args, **kwargs)
+        if blogpost.is_published:
+            blogpost.views_count += 1
+            blogpost.save(update_fields=['views_count'])
+            return blogpost
+        else:
+            return blogpost
+
+
+class BlogPostCreateView(CreateView):
+    model = BlogPost
+    template_name = 'blog/write_blog.html'
+    fields = ['title', 'category', 'content', 'preview']
+
+    def get_context_data(self, **kwargs):
+        context = {
+            'categories': Category.objects.all(),
+        }
+        return context
+
+    def form_valid(self, form):
+        self.object = form.save()
+        return render(self.request, "blog/blog_detail.html")
