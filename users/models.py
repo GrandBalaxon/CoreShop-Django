ab77e84146db42e1f8cb59ebbfb7495f17338bda
Vlad Spinul <shinokakaov@mail.ru>
2026-05-17 02:33:27 +0300

    feat: added CustomUser model + migration

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    country = models.CharField(max_length=50, blank=True, null=True, verbose_name="Страна")
+    phone_number = models.CharField(max_length=15, blank=True, null=True, verbose_name="Номер телефона")
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True, verbose_name="Аватар")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username',]
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        db_table = 'users'
+        verbose_name = "пользователь"
+        verbose_name_plural = "пользователи"
