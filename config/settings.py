40733d88563def35b8a738d34b2ea3b9c4de9c6b
Vlad Spinul <shinokakaov@mail.ru>
2026-05-17 16:20:37 +0300

    feat: added login.html + fixed routing + fixed menu.html

--- config/settings.py
+++ config/settings.py
@@ -111,4 +111,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Custom users
 AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = ''
+LOGIN_REDIRECT_URL = 'catalog:products_list'
--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path
 
 from users.apps import UsersConfig
@@ -7,4 +8,6 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
+    path("login/",LoginView.as_view(template_name="users/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(next_page='/'), name="logout"),
 ]
