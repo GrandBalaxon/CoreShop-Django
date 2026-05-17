e9297108101887fe400738af9a74236fa863386c
Vlad Spinul <shinokakaov@mail.ru>
2026-05-18 00:33:44 +0300

    feat: tweaked new urls + added new views + added new menu buttons

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import RegisterView, CustomUserDetailsView
+from users.views import RegisterView, ProfileOverviewView, ProfileProductsView, ProfileBlogsView
 
 app_name = UsersConfig.name
 
@@ -10,5 +10,7 @@ urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/",LoginView.as_view(template_name="users/login.html"), name="login"),
     path("logout/", LogoutView.as_view(next_page='/'), name="logout"),
-    path("profile/", CustomUserDetailsView.as_view(), name="profile"),
+    path("profile/", ProfileOverviewView.as_view(), name="profile"),
+    path("profile/products/", ProfileProductsView.as_view(), name="profile_products"),
+    path("profile/blogs/", ProfileBlogsView.as_view(), name="profile_blogs"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView, DetailView
+from django.db.models import Sum
+from django.views.generic import CreateView, DetailView, ListView
 
+from blog.models import BlogPost
+from catalog.models import Product
 from users.forms import CustomUserCreationForm
 from users.models import CustomUser
 
@@ -11,10 +14,37 @@ class RegisterView(CreateView):
     success_url = '/'
 
 
-class CustomUserDetailsView(LoginRequiredMixin, DetailView):
+class ProfileOverviewView(LoginRequiredMixin, DetailView):
     model = CustomUser
-    template_name = 'users/profile.html'
+    template_name = 'users/profile_overview.html'
     context_object_name = 'profile_user'
 
     def get_object(self, queryset=None):
         return self.request.user
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+
+        # Количество товаров
+        context['products_count'] = Product.objects.filter(owner=user).count()
+
+        # Сумма просмотров всех статей пользователя
+        total_views = BlogPost.objects.filter(author=user).aggregate(total=Sum('views_count'))['total'] or 0
+        context['total_blog_views'] = total_views
+
+        # Последний товар
+        context['last_product'] = Product.objects.filter(owner=user).order_by('-created_at').first()
+
+        # Последняя статья
+        context['last_blogpost'] = BlogPost.objects.filter(author=user).order_by('-created_at').first()
+
+        return context
+
+
+class ProfileProductsView(ProfileOverviewView):
+    template_name = 'users/my_products.html'
+
+
+class ProfileBlogsView(ProfileOverviewView):
+    template_name = 'users/my_blogs.html'
